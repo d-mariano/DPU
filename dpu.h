e70d9a2e8600be1062eec946b01d37dcb8f0b557
Dave Mariano <dave.mariano1991@gmail.com>
2015-02-25 18:57:53 -0500

    Added global definitionss

--- dpu.h
+++ dpu.h
@@ -6,19 +6,25 @@
  *  dpu.c function prototypes
  **********************************************/
 
-#define MEM_SIZE    (16 * 1024)
-#define BUFF_SIZE   512
-#define CHOICE_SIZE 3
-#define BYTE_SIZE   1
-#define forever     for(;;)
+#define MEM_SIZE        (16 * 1024)
+#define BUFF_SIZE       512
+#define CHOICE_SIZE     3
+#define BYTE_SIZE       ((int)sizeof(char))
+#define LINE_LENGTH     0x10
+#define INPUT_SIZE      4
+#define ZERO            48
+#define NINE            57
+#define forever         for(;;)
 
 int dpu_start();
 
 int dpu_go();
 
-int dpu_dump();
+int dpu_dump(void * memptr, unsigned int offset, unsigned int length);
 
 int dpu_LoadFile(void * memory, unsigned int max);
+
+int dpu_modify(void * memory, unsigned int offset);
 
 int dpu_quit();
 
